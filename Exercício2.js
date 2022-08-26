6b730cc3e8afae5335aebbce364c0b7372edc8cb
arthurtmd <112144472+arthurtmd@users.noreply.github.com>
2022-08-26 12:27:48 -0300

    Update Exercício2.js

--- Exercício2.js
+++ Exercício2.js
@@ -1,20 +1,61 @@
-let ax2 = 3
-let bx = -15
-let c = 12
+const ax2 = document.getElementById("ax2");
+const bx = document.getElementById("bx");
+const c = document.getElementById("c");
+const x1 = document.getElementById("x1");
+const x2 = document.getElementById("x2");
+const btn = document.querySelector("#calcular")
+const campoax2 = document.getElementById("campoax2")
+const campobx = document.getElementById("campobx")
+const campox = document.getElementById("campox")
 
-let delta = ((bx*bx)-4*ax2*c)
+// Identificar não preenchimento de campo
 
-if (delta < 0) {
+ax2.onblur = () => {
+		if (ax2.value == "" || ax2.value == "0") {
+    		campoax2.style = "color: #ff0000";
+    } else {
+    		campoax2.style = "color: #000000";
+    }
+}
 
-let deltaNegativo = "Delta é negativo"
-console.log(deltaNegativo)
+bx.onblur = () => {
+		if (bx.value == "") {
+    		campobx.style = "color: #ff0000";
+    } else {
+    		campobx.style = "color: #000000";
+    }
+}
 
-} else {
+c.onblur = () => {
+		if (c.value == "") {
+    		campoc.style = "color: #ff0000";
+    } else {
+    		campoc.style = "color: #000000";
+    }
+}
 
-let x1 = ((- bx + Math.sqrt(delta)) / (2 * ax2))
-let x2 = ((- bx - Math.sqrt(delta)) / (2 * ax2))
-let string = "Os resultados são: " + x1 + " e " + x2
+// Cálculo
 
-console.log(string)
+btn.onclick = () => {
+	if (ax2.value == "" || ax2.value == "0"){
+  	ax2.focus();
+  }else if (bx.value == "") {
+    	bx.focus();
+  } else if (c.value == ""){
+    	c.focus();
+  } else {
+  
+  const delta = (bx.value*bx.value)-4*ax2.value*c.value;
+  
+  if (delta < 0) {  
+x1.value = "Delta é negativo!";
+x2.value = ""; 
 
+}
+else {
+  x1.value = (-bx.value + Math.sqrt(delta)) / (2*ax2.value);
+  x2.value = (-bx.value - Math.sqrt(delta)) / (2*ax2.value);
+}
+
+}
 }
